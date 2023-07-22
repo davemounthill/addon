089ff7db9440602220ab0e3c02816868e65eda43
davemounthill <90378859+davemounthill@users.noreply.github.com>
2023-07-22 22:33:56 +0200

    Delete utils directory

--- utils/runtypesRegistry.ts
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.RuntypesRegistry = void 0;
-var typed_apis_1 = require("@syncwith/typed-apis");
-exports.RuntypesRegistry = new typed_apis_1.Registry();
